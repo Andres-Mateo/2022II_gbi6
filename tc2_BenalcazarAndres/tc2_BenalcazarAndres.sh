35a93b06cb50bb5a9c5cbba50589f8d2723051c7
Andres-Mateo <105082574+Andres-Mateo@users.noreply.github.com>
2022-11-09 02:32:00 -0500

    Tarea 2

---
+++ tc2_BenalcazarAndres/tc2_BenalcazarAndres.sh
@@ -0,0 +1,9 @@
+#! Bin Bash
+
+echo "Primeras 7 filas"
+
+head -n 7 EigenBetw.tsv
+
+echo "Ultimas 5 filas"
+
+tail -n 5 EigenBetw.tsv
---
+++ tc2_BenalcazarAndres/tc2_BenalcazarAndres2.sh
@@ -0,0 +1,4 @@
+# ! bin bash
+
+
+grep AGAP[0-9]*[5], EigenBetw.tsv | sort -k 3 > EigenBetw5.csv
